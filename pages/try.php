117f70c8e14c3cd3a7ddcd3996a280621f4d1a89
arunimavs <arunimavs777@gmail.com>
2017-06-21 16:28:45 +0530

    html-pages

---
+++ pages/try.php
@@ -0,0 +1,32 @@
+
+ <?php
+
+echo "\n";
+
+
+$x = $argv[1];
+
+
+echo "\n";
+
+
+
+ while($x>=5) {
+   echo  "$x\n\n";
+    $x--;
+ } 
+
+
+
+ $colors = array("red", "green", "blue", "yellow");
+
+for ($i=0;$i<4;$i++) {
+	$a[$i]=readline();
+   
+}
+for ($i=0;$i<4;$i++) {
+	echo "\n$a[$i]";
+   
+}
+?>
+
